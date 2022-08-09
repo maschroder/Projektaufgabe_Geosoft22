294e2dfca5e542c95bcd7519219d72485700ebee
derya-sen <82390935+derya-sen@users.noreply.github.com>
2022-08-09 14:32:49 +0200

    gebirge

--- script.js
+++ script.js
@@ -6,3 +6,29 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
+
+var gebirge = {
+  "type": "FeatureCollection",
+  "features": [
+      {
+          "type": "Feature",
+          "properties": {
+              "name": "Mont Blanc",
+              "hoehe": "4810m",
+              "url" : "https://de.wikipedia.org/wiki/Mont_Blanc",
+              "beschreibung" : "Berg an der Grenze zwischen Frankreich und Italien"
+
+          },
+          "geometry": {
+              "type": "Point",
+              "coordinates": [
+                  6.864325,
+                  45.832544
+              ]
+          }
+      
+      }
+  ]
+}
+
+L.geoJSON(gebirge).addTo(map);
