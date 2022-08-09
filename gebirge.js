934e9e79c7b384ec5034f78187f2529aa419f792
maschroder <mais1998@web.de>
2022-08-09 12:43:20 +0200

    gebirge

---
+++ gebirge.js
@@ -0,0 +1,23 @@
+var gebirge = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "name": "Mont Blanc",
+                "hoehe": "4810m",
+                "url" : "https://de.wikipedia.org/wiki/Mont_Blanc",
+                "beschreibung" : "Berg an der Grenze zwischen Frankreich und Italien"
+
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    6.864325,
+                    45.832544
+                ]
+            },
+        
+        },
+    ]
+}
