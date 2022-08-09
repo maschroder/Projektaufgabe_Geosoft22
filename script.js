d7c95a71e5827ac06d2f3817285d96a8690fddd7
maschroder <mais1998@web.de>
2022-08-09 13:29:37 +0200

    abc

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-var map = L.map('map',{drawControl: false}, {editable: true}).setView([51.96, 7.63], 12);
+"use strict";
+var map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
 }).addTo(map);
