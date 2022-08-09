de5f8582a1c1dd2399f8df1c217a6578e66f8fa2
derya-sen <82390935+derya-sen@users.noreply.github.com>
2022-08-09 13:26:25 +0200

    map

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
- // create a variable for the map
- let mymap = L.map('myfirstmap',
- {
-     center: [51.961563, 7.628202],
-     zoom: 13
- }) 
+var map = L.map('map').setView([51.96, 7.62], 13);
 
-   
-// add the base map
-L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${myMapBoxKey}`, {
- attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
- maxZoom: 18,
- id: 'mapbox/streets-v11',
- tileSize: 512,
- zoomOffset: -1,
- accessToken: myMapBoxKey
-}).addTo(mymap)
+// Add base tiles
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 17,
+    minZoom: 3
+}).addTo(map);
