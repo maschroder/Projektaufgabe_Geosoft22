a240b58e84be60b9336c0a886d4443ec572b6a46
derya-sen <82390935+derya-sen@users.noreply.github.com>
2022-08-12 13:30:44 +0200

    hoehe

--- public/javascripts/create.js
+++ public/javascripts/create.js
@@ -72,7 +72,7 @@ class MapInterface{
             <input id="name" name="name" value="" placeholder="name">
             <input type="hidden" id="lat" name="lat" value="${layer._latlng.lat}">
             <input type="hidden" id="lng" name="lng" value="${layer._latlng.lng}">
-            <input id="hoehe" name="höhe" value="" placeholder="höhe">
+            <input id="hoehe" name="hoehe" value="" placeholder="hoehe">
             <input id="beschreibung" name="beschreibung" value="" placeholder="beschreibung">
             <input id="url" name="url" value="" placeholder="url">
             <input type="submit" value="Submit">
