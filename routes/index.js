8fc23d952ea50fb38348c0525df1c9617f0e3124
derya-sen <82390935+derya-sen@users.noreply.github.com>
2022-08-12 13:28:15 +0200

    gebirge

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  gebirgeId = req.query.id || false; //kurz für "entweder req.params.poiId, oder false wenn es undefined ist."
+  gebirgeId = req.query.id || false; //kurz für "entweder req.params.gebirgeId, oder false wenn es undefined ist."
   res.render('index', { title: 'Home', gebirgeID: gebirgeId });
 });
 
