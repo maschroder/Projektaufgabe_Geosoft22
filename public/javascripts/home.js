ee2850fdfc02e40fe698609a798ec841bff15ac7
maschroder <mais1998@web.de>
2022-09-05 14:04:01 +0200

    button für standort

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -171,6 +171,62 @@ class MapInterface{
   }
 }
 
+
+
+/**
+ * @function showPosition
+ * @desc Shows the position of the user in the textarea
+ * @param {*} position Json object of the user
+ */
+ function showPosition(position) {
+  var x = document.getElementById("userPosition");
+  //"Skeleton" of a valid geoJSON Feature collection
+  let outJSON = { "type": "FeatureCollection", "features": [] };
+  //skelly of a (point)feature
+  let pointFeature = {"type": "Feature","properties": {},"geometry": {"type": "Point","coordinates": []}};
+  pointFeature.geometry.coordinates = [position.coords.longitude, position.coords.latitude];
+  //add the coordinates to the geoJson
+  outJSON.features.push(pointFeature);
+  x.innerHTML = JSON.stringify(outJSON);
+}
+
+// Funktionen für Abgabe 3 und 4. Die Funktionen hierüber in der script.js stammen aus der Musterlösung der 2. Abgabe
+
+
+
+function getLocation() {
+  x = document.getElementById("location");
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+
+}
+
+function showPosition(position) {
+  var lat = position.coords.latitude;
+  var lng = position.coords.longitude;
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+
+  myLocation = [lat,lng]
+  // adding a green icon 
+  var greenIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+console.log(myLocation)
+ new L.marker(myLocation, {icon: greenIcon}).addTo(map).bindPopup("Ich bin hier");
+}
+
+
+
+
 const mainMapInterface = new MapInterface(
   {
     mapid: "map",
