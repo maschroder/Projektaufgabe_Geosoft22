77bd49f152ebcb5c63cbe865ee48ef998b012042
maschroder <mais1998@web.de>
2022-09-07 13:36:28 +0200

    neu

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -2,11 +2,8 @@
 var x = document.getElementById("demo");
 var bigOnes = ["Der Mont Blanc mit 4810 Metern!", "Dufourspitze mit 4634 Metern!"];
 function showBiggest(){
-  console.log("Der Mont Blanc mit 4810 Metern!")
   document.getElementById("biggestMountain").innerHTML = bigOnes[0];
 }
-
 function showSecondBiggest(){
-  console.log("Der Mont Blanc mit 4810 Metern!")
   document.getElementById("secondBiggestMountain").innerHTML = bigOnes[1];
 }
